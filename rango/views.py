d3cc716a126d5f9056a14355ea7ece8f04b41691
PeterMai <89789068+IsPeterHere@users.noreply.github.com>
2025-01-24 18:25:32 +0000

    chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 def index(request):
-   return HttpResponse("Rango says hey there partner!")
+   context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+   return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+   return render(request, 'rango/about.html')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Application definition
 
@@ -52,10 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,10 +66,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', 
             ],
         },
     },
 ]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
